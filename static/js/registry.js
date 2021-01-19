2060547df75e9011028710e4b4fde767e6dbf8e8
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2021-01-19 19:17:51 +0200

    EDP-DDM-2112 start to integrate datatables

--- static/js/registry.js
+++ static/js/registry.js
@@ -44,4 +44,32 @@ $(function (){
     $("#delete-form").submit(function () {
         return registryName === registryInput.val()
     });
+
+
+});
+
+$(document).ready(function() {
+    $("#registry-table").DataTable({
+        ordering: true,
+        paging: true,
+        language: {
+            "processing": "Зачекайте...",
+            "lengthMenu": "Показати _MENU_ записів",
+            "zeroRecords": "Записи відсутні.",
+            "info": "Записи з _START_ по _END_ із _TOTAL_ записів",
+            "infoEmpty": "Записи з 0 по 0 із 0 записів",
+            "infoFiltered": "(відфільтровано з _MAX_ записів)",
+            "search": "Пошук:",
+            "paginate": {
+                "first": "Перша",
+                "previous": "Попередня",
+                "next": "Наступна",
+                "last": "Остання"
+            },
+            "aria": {
+                "sortAscending": ": активувати для сортування стовпців за зростанням",
+                "sortDescending": ": активувати для сортування стовпців за спаданням"
+            }
+        }
+    });
 });
