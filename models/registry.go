016191c19059863a1ef1a2fcbef11df71cd9bd03
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2021-04-05 17:17:03 +0300

    [EDP-DDM-4255]: fix admins regexp

--- models/registry.go
+++ models/registry.go
@@ -11,7 +11,7 @@ const viewTimeFormat = "02.01.2006 15:04"
 type Registry struct {
 	Name        string `form:"name" valid:"Required;Match(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?([a-z0-9]([-a-z0-9]*[a-z0-9])?)*$/);MinSize(3);MaxSize(12)"`
 	Description string `form:"description" valid:"MaxSize(250)"`
-	Admins      string `form:"admins" valid:"Match(/^[a-zA-Z0-9@._-,]+$/)"`
+	Admins      string `form:"admins" valid:"Match(/(^[a-zA-Z0-9@._,-]+$)|(^$)/)"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	NS          *v1.Namespace
