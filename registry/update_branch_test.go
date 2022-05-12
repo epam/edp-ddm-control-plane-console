7c2b6ef2b393ae86d2b7db10c958a8e687d74bb7
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2022-05-12 18:35:58 +0300

    [EDP-DDM-00000]: fix branch version parsing

--- registry/update_branch_test.go
+++ registry/update_branch_test.go
@@ -13,7 +13,7 @@ func TestSortByVersion(t *testing.T) {
 	assert.Equal(t, in,
 		[]string{"1.5.6-SNAP-100", "1.5.6-SNAP-200", "1.5.6-SNAP-208", "1.5.6-SNAP-210", "1.5.6-SNAP-300"})
 
-	in = []string{"1.5.6-SNAPSHOT.401", "1.6.1", "1.5.6-SNAPSHOT.400"}
+	in = []string{"1.5.6-SNAPSHOT.401", "1.6.1", "1.5.6-SNAPSHOT.400", "master"}
 	sort.Sort(SortByVersion(in))
-	assert.Equal(t, in, []string{"1.5.6-SNAPSHOT.400", "1.5.6-SNAPSHOT.401", "1.6.1"})
+	assert.Equal(t, in, []string{"master", "1.5.6-SNAPSHOT.400", "1.5.6-SNAPSHOT.401", "1.6.1"})
 }
