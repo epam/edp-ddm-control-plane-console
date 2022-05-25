3402acae1c87c61f52a0e27673ac8a8467d58f65
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2022-05-25 19:12:26 +0300

    [EDP-DDM-13097]: add failed status to external reg on MR failure

--- app/registry/external_reg.go
+++ app/registry/external_reg.go
@@ -22,6 +22,7 @@ const (
 	externalSystemTypeExternal = "external-system"
 	erValuesIndex              = "nontrembita-external-registration"
 	erStatusInactive           = "inactive"
+	erStatusFailed             = "failed"
 	erStatusActive             = "active"
 	erStatusDisabled           = "disabled"
 	MRLabelType                = "mr/type"
