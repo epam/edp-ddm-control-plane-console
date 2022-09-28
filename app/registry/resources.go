6396199621d7dbb202bce6d4ebc09eda5113f136
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2022-09-28 15:32:09 +0300

    [EDP-DDM-18657]: implement registry resources on backend

--- app/registry/resources.go
+++ app/registry/resources.go
@@ -1,5 +1,28 @@
 package registry
 
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
 func (a *App) prepareRegistryResources(r *registry, values map[string]interface{}) error {
+	if r.Resources != "" {
+		var resources map[string]interface{}
+		if err := json.Unmarshal([]byte(r.Resources), &resources); err != nil {
+			return errors.Wrap(err, "unable to decode resources")
+		}
+
+		global, ok := values["global"]
+		if !ok {
+			global = make(map[string]interface{})
+		}
+
+		globalDict := global.(map[string]interface{})
+
+		globalDict["registry"] = resources
+		values["global"] = globalDict
+	}
+
 	return nil
 }
