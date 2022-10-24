263ca5b92fda1698f1509cc3f5e50ada09c13e51
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2022-10-24 14:28:44 +0300

    [EDP-DDM-19772]: fix view change route

--- app/registry/routes.go
+++ app/registry/routes.go
@@ -18,6 +18,5 @@ func (a *App) createRoutes() {
 	a.router.POST("/admin/registry/external-reg-add/:name", a.addExternalReg)
 	a.router.POST("/admin/registry/external-reg-remove/:name", a.removeExternalReg)
 	a.router.POST("/admin/registry/external-reg-disable/:name", a.disableExternalReg)
-
-	a.router.GET("/admin/registry/:name/change/:change", a.viewChange)
+	a.router.GET("/admin/change/:change", a.viewChange)
 }
