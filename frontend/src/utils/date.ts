a52231cdb084037e5945054188f012a36a38c61d
Mykyta_Holkin <mykyta_holkin@epam.com>
2023-05-05 18:58:56 +0300

    [EDP-DDM-25420] mr table sorting fix
    Change-Id: Ie157f756889ff7897bb78575216827ed6747f9d6

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -3,3 +3,7 @@ import moment from "moment";
 export const getFormattedDate = (date: string): string => {
     return moment(date).format('DD.MM.YYYY h:mm');
 };
+
+export const getDateTimestamp = (date: string): number => {
+  return moment(date).unix();
+};
