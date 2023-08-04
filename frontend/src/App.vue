cc69e9045b28286952a497c884e39dc9246b5e85
Ruslan Bielyi <ruslan_bielyi@epam.com>
2023-08-04 17:55:01 +0300

    [EDP-DDM-27420] fix for broken yup url validation
    Change-Id: I605dbeb84860fc3d4455dde329b40d4b52fb0a9c

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,20 @@ import { RouterView } from 'vue-router';
 </script>
 
 <script lang="ts">
-import { setLocale } from 'yup';
+import * as Yup from 'yup';
 
-setLocale({
+Yup.setLocale({
   mixed: {
     required: 'required',
   },
+});
+
+// Override the original URL validation
+// workaround for Yup issue for urls that start with '//'
+// https://github.com/jquense/yup/issues/672
+const originalUrl = Yup.string().url;
+Yup.addMethod(Yup.string, 'url', function (...args) {
+  return originalUrl.call(this, ...args).test((value => !value?.startsWith('//')));
 });
 </script>
 
